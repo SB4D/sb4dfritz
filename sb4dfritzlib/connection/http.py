3eb639f850f5a0ab24b25effa2a6f71b7b3cc301
SB4D <sbehrens4d@gmail.com>
2025-08-19 09:23:27 +0200

    added file-based doc strings

--- sb4dfritzlib/connection/http.py
+++ sb4dfritzlib/connection/http.py
@@ -8,11 +8,7 @@ AHA = 'webservices/homeautoswitch.lua'
 DATA = 'data.lua'
 
 def aha_request(params:dict[str:str])->requests.Response:
-    # ain = AINS[0].replace(" ", "")              # AIN of your smart device
-    # # cmd = 'getswitchlist'               # Example command (see command list below)
-    # cmd = 'getswitchpower'               # Example command (see command list below)
-    # cmd = 'getbasicdevicestats'               # Example command (see command list below)
-
+    """Basa HTTP GET request for the AHA-HTTP interface."""
     # Parameters for the GET request
     params = [f"{key}={val}" for key, val in params.items()]
     params = "&".join(params)
@@ -20,3 +16,8 @@ def aha_request(params:dict[str:str])->requests.Response:
     request_url = f"{URL_BASE}{AHA}?{params}"
     response = requests.get(request_url, verify=False)  # Use verify=False if self-signed cert
     return response
+
+# ain = AINS[0].replace(" ", "")              # AIN of your smart device
+# # cmd = 'getswitchlist'               # Example command (see command list below)
+# cmd = 'getswitchpower'               # Example command (see command list below)
+# cmd = 'getbasicdevicestats'               # Example command (see command list below)
---
+++ sb4dfritzlib/homeauto/__init__.py
@@ -0,0 +1,3 @@
+"""Provides a framework for object-based interaction with
+home automation devices within an AVM FRITZ!Box network."""
+
